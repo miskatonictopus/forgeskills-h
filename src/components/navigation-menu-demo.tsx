9dd2cc9c7b2a1907db51b53734c8303bb93ae9a6
Javier Ferrer <javier.ferrer@ifp.es>
2025-10-07 23:11:34 +0200

    menu

--- src/components/navigation-menu-demo.tsx
+++ src/components/navigation-menu-demo.tsx
@@ -62,7 +62,7 @@ export function NavigationMenuDemo() {
             <ul className="grid gap-2 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
               <li className="row-span-3">
                 <NavigationMenuLink asChild>
-                  <a
+                <Link
                     className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                     href="/"
                   >
@@ -70,7 +70,7 @@ export function NavigationMenuDemo() {
                     <p className="text-sm leading-tight text-muted-foreground">
                       Beautifully designed components built with Tailwind CSS.
                     </p>
-                  </a>
+                  </Link>
                 </NavigationMenuLink>
               </li>
               <ListItem href="/docs" title="Introduction">
